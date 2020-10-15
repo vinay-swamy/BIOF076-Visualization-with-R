e788daa7aaae169f88465f0df522c82b6bbed9df
Swamy <swamyvinny@gmail.com>
2020-10-14 22:38:41 -0400

    Files re arranged to reflect material covered in first run of the workshop. Material I wasnt able to cover has been moved to extra_lessons

--- shiny_examples/basic_app.R
+++ shiny_examples/basic_app.R
@@ -8,19 +8,19 @@ ui <- fluidPage(
         # Sidebar panel for inputs ----
         sidebarPanel(
             # Input: Slider for the number of bins ----
-            sliderInput(inputId = "bins", label = "Number of bins:", min = 1,max = 50, value = 30)
+            sliderInput(inputId = "bin_output", label = "Number of bins:", min = 1,max = 50, value = 30)
         ),
         # Main panel for displaying outputs ----
         mainPanel(
             # Output: Histogram ----
-            plotOutput(outputId = "distPlot")
+            plotOutput(outputId = "hist_out")
         )
     )
 )
 server <- function(input, output) {
-    output$distPlot <- renderPlot({ 
+    output$hist_out <- renderPlot({ 
         x    <- faithful$waiting
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        bins <- seq(min(x), max(x), length.out = input$bin_output + 1)
         
         hist(x, breaks = bins, col = "#75AADB", border = "white",
              xlab = "Waiting time to next eruption (in mins)",
