1cf69265b33fada4bc049bf87fa477cbe1df4166
Swamy <swamyvinny@gmail.com>
2021-08-11 17:09:05 -0700

    cleaned up 6/7 folders. Will skim through Rmarkdown lesson, but likely wont change much. Should be able to put the finishing touches tomorrow

--- src/make_fake_sc_data.R
+++ src/make_fake_sc_data.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 data <- read_tsv('src/pbmc_cell_expression.tsv.gz')
-keep_side <- data[,1:3]
+keep_side <- data[,c(1:3, 5:6)]
 pid_resample <- tibble(patient_id = sample(data$patient_id, size = nrow(data), replace = T))
 gexp_resample <- lapply(5:14, function(x) rlnorm(nrow(data), sdlog = sample(1:10, 1)) ) %>% do.call(cbind, .) %>% as.data.frame
-colnames(gexp_resample) <- colnames(data)[-(1:4)] 
+colnames(gexp_resample) <- colnames(data)[-(1:6)] 
 res <- bind_cols(keep_side, pid_resample, gexp_resample)
 write_tsv(res, paste0( 'sc_data_', paste0(sample(LETTERS,size = 5, replace = T ), collapse = ''), '.tsv' ))
           
